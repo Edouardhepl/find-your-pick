2f46b1b96acb961e0be4b37db6826a44e9e6b283
edouardhepl <edouard.dradin@student.hepl.be>
2024-05-08 12:07:31 +0200

    css

--- projet/index.php
+++ projet/index.php
@@ -10,41 +10,39 @@ ensuite require once de register et login et si connecté alors afficher le form
 
 sql : requêtes pour les listes déroulantes (moodle)
 <!DOCTYPE html>
-    <html>
-        <head>
-            <meta charset="utf-8" />
-            <title>Mon site</title>
-            <link rel="stylesheet" href="css/style.css">
-        </head>
-        <header><?php require_once(__DIR__ . '/src/partials/header.php'); ?></header>
-        <body>
-            <form method="POST">
-                <div>
-                    <label for="champions">Champions</label>
-                    <select name="id_champion">
-                    </select>
-                </div>
-                <div>
-                    <label for="playstyle">Style de jeu</label>
-                    <select name="playstyle">
-                    </select>
-                </div>
-                <div>
-                    <label for="type">Type du champion</label>
-                    <select name="type">
-                        // requête sql 
-                    </select>
-                </div>
-                <div>
-                    <label for="lane">Lane</label>
-                    <select name="lane">
-                        // requête sql 
-                    </select>
-                </div>
-                <button type="submit">Soumettre</button>
-            </form>
-        </body>
-        <footer>
-            <?php require_once(__DIR__ . '/src/partials/footer.php'); ?>
-        </footer>
-    </html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Mon site</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header><?php require_once(__DIR__ . '/src/partials/header.php'); ?></header>
+    <form method="POST" class="form-container">
+        <div class="form-group">
+            <label for="champions">Champions</label>
+            <select name="id_champion" class="form-control">
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="playstyle">Style de jeu</label>
+            <select name="playstyle" class="form-control">
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="type">Type du champion</label>
+            <select name="type" class="form-control"> 
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="lane">Lane</label>
+            <select name="lane" class="form-control">
+            </select>
+        </div>
+        <button type="submit" class="btn-submit">Soumettre</button>
+    </form>
+    <footer>
+        <?php require_once(__DIR__ . '/src/partials/footer.php'); ?>
+    </footer>
+</body>
+</html>
